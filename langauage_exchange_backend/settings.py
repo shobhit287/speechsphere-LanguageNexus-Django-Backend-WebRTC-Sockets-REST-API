b4be2b656b0593b7398a90d18d2b021233cbe10c
Shobhiti7 <shobhit.sharma@i7infomedia.com>
2024-05-10 23:28:03 +0530

    fix

--- langauage_exchange_backend/settings.py
+++ langauage_exchange_backend/settings.py
@@ -132,12 +132,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-REDIS_URL = os.getenv('REDIS_URL')
 CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [REDIS_URL],
-        },
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer',
     },
 }
